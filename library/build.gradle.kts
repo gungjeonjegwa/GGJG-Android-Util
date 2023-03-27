4ce501e18b2e87a1dbf9c1d9c3e0e04bc1c2272f
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-03-27 15:48:27 +0900

    versionCode 추가

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -11,8 +11,11 @@ android {
     compileSdk = 33
 
     defaultConfig {
+        applicationId = "com.ggjg.library"
         minSdk = 28
         targetSdk = 33
+        versionCode = 1
+        versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
