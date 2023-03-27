4e59401c123d6fa0bb20b3e3863bca97c35de2ae
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-03-27 15:19:48 +0900

    exception 정리

---
+++ api_call/src/main/java/com/ggjg/api_call/handler/exceion/HttpException.kt
@@ -0,0 +1,11 @@
+package com.ggjg.api_call.handler.exceion
+
+class MoreDataException : RuntimeException()
+
+class TokenErrorException : RuntimeException()
+
+class NotFoundException : RuntimeException()
+
+class ConflictException : RuntimeException()
+
+class UnknownException : RuntimeException()
---
+++ api_call/src/main/java/com/ggjg/api_call/handler/exceion/IOEception.kt
@@ -0,0 +1,5 @@
+package com.ggjg.api_call.handler.exceion
+
+import java.io.IOException
+
+class NeedLoginException : IOException()
